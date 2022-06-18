0f0b3a68a5df35c4ac869aeb5c7ff38a02ba3862
Nick Matthews <matthews3102000@gmail.com>
2022-06-18 08:18:33 -0400

    Update surfsetup.sh

--- surfsetup.sh
+++ surfsetup.sh
@@ -12,6 +12,7 @@ sudo wget https://my.surfshark.com/vpn/api/v1/server/configurations #This downlo
 
 sudo unzip configurations #This unzips the configurations zip
 echo -e "\e[0;31m PLEASE READ THE FOLLOWING \e[0m"
+echo "Type cd and now type cd /etc/openvpn"
 echo "Type ls and then chose a location from the configurations files."
 echo "Now type sudo openvpn 'paste the file name here' and hit enter."
 echo "From here you need to enter you credentials which can be found under VPN --> Manual setup --> Credentials."
